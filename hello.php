f15867a8c1895a4233633ec3c846eddf53be994e
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2019-05-22 11:13:41 +0700

    hello php

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+	<link rel="stylesheet" type="text/css" href="hello.css">
+</head>
+<body>
+	<p>Today is 
+        <span id="date"> <?php 
+          echo  date('l');
+         ?>
+         </span>
+</p>
+</body>
+</html>
